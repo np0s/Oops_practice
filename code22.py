ce31cbe03ea9e3d9ba06f512d82e0f15f1ef724d
np0s <kavyak10b@gmail.com>
2026-02-09 13:52:02 +0530

    2 lines

--- code22.py
+++ code22.py
@@ -1,4 +1,7 @@
-for i in range(1,6):
-    for j in range(1,i+1):
-        print("a",end="")
-    print()
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print("a",end="")
+#     print()
+
+for i in range(6,0,-1):
+    print("a"*i)
